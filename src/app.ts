31a3e3acfb0780f8010643f76804e2c3284dc532
kalilistic <35899782+kalilistic@users.noreply.github.com>
2022-05-22 10:45:28 -0400

    refactor: extract routes

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,14 @@
 import express from "express";
 
 const app = express();
+const router = require('./router/router');
 
 const cors = require('./middleware/cors');
 const options = require('./middleware/options');
 
-const characterController = require('./controller/character.controller');
-const freeCompanyController = require('./controller/freeCompany.controller');
-
 export default function () {
-
   app.use(cors);
   app.use(options);
-
-  app.get("/character/search", characterController.search);
-  app.get("/character/:id", characterController.get);
-  app.get("/free-company/search", freeCompanyController.search);
-  app.get("/free-company/:id", freeCompanyController.get);
-
+  app.use(router);
   return app;
 }
